991a83ac1dad64e128a624244cd84c6c1f628d39
apuyo <apuyo12589@gmail.com>
2024-10-19 16:33:43 +0200

    Arreglada la lectura del csv

--- src/main/java/com/UmbrellaCorp/UmbrellaCorporation/CSVController.java
+++ src/main/java/com/UmbrellaCorp/UmbrellaCorporation/CSVController.java
@@ -1,5 +1,6 @@
 package com.UmbrellaCorp.UmbrellaCorporation;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +15,11 @@ public class CSVController {
     @Autowired
     private LectorDatosCSV lectorDatosCSV;
 
-    @GetMapping("/procesar-csv")
-    public List<String[]> procesarCSV(@RequestParam String filePath) {
+    @GetMapping("/datos-csv")
+    public List<String[]> procesarCSV(@RequestParam(required = false) String filePath) {
+        String path = (filePath != null) ? filePath : "src/res/gifted_iq_data.csv"; // Usa la ruta por defecto si no se proporciona
         try {
-            return lectorDatosCSV.leerCSVConcurrencia(filePath);
+            return lectorDatosCSV.leerCSVConcurrencia(path);
         } catch (Exception e) {
             e.printStackTrace();
             return Collections.emptyList();
--- src/main/java/com/UmbrellaCorp/UmbrellaCorporation/UmbrellaCorporationApplication.java
+++ src/main/java/com/UmbrellaCorp/UmbrellaCorporation/UmbrellaCorporationApplication.java
@@ -2,12 +2,37 @@ package com.UmbrellaCorp.UmbrellaCorporation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class UmbrellaCorporationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmbrellaCorporationApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void openBrowser() {
+		String os = System.getProperty("os.name").toLowerCase();
+		try {
+			if (os.contains("win")) {
+				// Windows
+				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080/datos-csv?filePath=src/res/gifted_iq_data.csv");
+			} else if (os.contains("mac")) {
+				// Mac
+				Runtime.getRuntime().exec("open http://localhost:8080/datos-csv?filePath=src/res/gifted_iq_data.csv");
+			} else if (os.contains("nix") || os.contains("nux")) {
+				// Linux
+				Runtime.getRuntime().exec("xdg-open http://localhost:8080/datos-csv?filePath=src/res/gifted_iq_data.csv");
+			} else {
+				System.err.println("Sistema operativo no soportado para abrir el navegador.");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
